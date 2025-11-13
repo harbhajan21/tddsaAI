88cd7ddc0cad7448866864e56ec6cdb77439b16e
Harbhajan Singh <38522435+harbhajan21@users.noreply.github.com>
2025-11-13 12:18:43 -0500

    Update imports for text splitter and chains

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ from phi.knowledge.pdf import PDFUrlKnowledgeBase
 from phi.vectordb.pgvector import PgVector2
 from phi.utils.log import logger
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+# from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
 from langchain_core.prompts import ChatPromptTemplate
@@ -25,8 +26,10 @@ from openai import OpenAI
 from langchain_openai import OpenAI
 # from langchain.chat_models import ChatOpenAI
 from langchain_community.chat_models import ChatOpenAI
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain
+# from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+# from langchain.chains import create_retrieval_chain
+from langchain_classic.chains import create_retrieval_chain
 
 # Security configuration
 LOGIN_CREDENTIALS = {"admin": "admin"}
